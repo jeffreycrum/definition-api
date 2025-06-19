9423123edfdf6d84b45ad0be06732c892e555d55
Jeff Crum <jeffrey.crum81@gmail.com>
2025-06-19 15:59:18 -0700

    api is now returning definition from csv file

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template
 import pandas as pd
+from flask import Flask, render_template
 
 app = Flask("__name__")
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/api/v1/<word>")
 def api(word):
-    definition = word.upper()
+    df = pd.read_csv("dictionary.csv")
+    definition = df.loc[df["word"] == word]["definition"].squeeze()
     result_dictionary = {"word": word, "definition": definition}
     return result_dictionary
 
